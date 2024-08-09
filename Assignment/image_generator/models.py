16bc310780882088d38f7a95ba8a2b9ab0f184ce
Sj Sujeet <135042019+sujeetsj@users.noreply.github.com>
2024-08-09 22:57:56 +0530

    Delete Assignment directory

--- Assignment/image_generator/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class ImageMetadata(models.Model):
-    text_prompt = models.CharField(max_length=255)
-    image_url = models.URLField()
-    created_at = models.DateTimeField(auto_now_add=True)
--- Assignment/image_generator/urls.py
+++
@@ -1,7 +0,0 @@
-# urls.py
-from django.urls import path
-from .views import generate_images
-
-urlpatterns = [
-    path('generate/', generate_images, name='generate_images'),
-]
